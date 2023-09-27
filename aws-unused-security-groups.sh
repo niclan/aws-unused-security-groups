db2a738b20056fe842dc2747d0f40c6b167e92d5
Nicolai Langfeldt <nicolai.langfeldt@schibsted.no>
2023-09-27 14:45:36 +0200

    Also list rds vpc security groups

--- aws-unused-security-groups.sh
+++ aws-unused-security-groups.sh
@@ -16,73 +16,93 @@ true> sg-unused-final.txt
 
 printf "\nlist unused security groups in all regions of an AWS account\n"
 
+# Get regions list once and for all
+REGIONS=$(aws ec2 describe-regions | jq --raw-output '.Regions[].RegionName')
+
 printf "\nstep 1: list all security groups "
-for region in $(aws ec2 describe-regions | jq --raw-output '.Regions[].RegionName'); do
-  printf "=";
+for region in $REGIONS; do
+  printf "="
   for sg in $(aws ec2 describe-security-groups --region $region | jq --raw-output '.SecurityGroups[].GroupId'); do
-    printf "%s\t%s\n" "$region" "$sg">>sg-all.txt; printf "-";\
-  done;
+    printf "%s\t%s\n" "$region" "$sg">>sg-all.txt
+    printf "-"
+  done
 done
 sort sg-all.txt | uniq >sg-all-sorted.txt
 wc -l sg-all-sorted.txt
 
 printf "\nstep 2: list default security groups (can't be deleted) "
-for region in $(aws ec2 describe-regions | jq --raw-output '.Regions[].RegionName'); do
-  printf "=";	
+for region in $REGIONS; do
+  printf "="
   for sg in $(aws ec2 describe-security-groups --region $region --filters Name=group-name,Values=default | jq --raw-output '.SecurityGroups[].GroupId'); do
-    printf "%s\t%s\n" "$region" "$sg">>sg-used-default.txt; printf "-";\
-  done;
+    printf "%s\t%s\n" "$region" "$sg">>sg-used-default.txt
+    printf "-"
+  done
 done
 wc -l sg-used-default.txt
 
 printf "\nstep 3: list security groups associated with instances "
-for region in $(aws ec2 describe-regions | jq --raw-output '.Regions[].RegionName'); do
-  printf "=";		
+for region in $REGIONS; do
+  printf "="		
   for sg in $(aws ec2 describe-instances --region $region | jq --raw-output '.Reservations[].Instances[].SecurityGroups[].GroupId'); do
-    printf "%s\t%s\n" "$region" "$sg">>sg-used-ec2.txt; printf "-";\
-  done;
+    printf "%s\t%s\n" "$region" "$sg">>sg-used-ec2.txt
+    printf "-"
+  done
 done
 wc -l sg-used-ec2.txt
 
 printf "\nstep 4: list security groups associated with network interfaces "
-for region in $(aws ec2 describe-regions | jq --raw-output '.Regions[].RegionName'); do
-  printf "=";	
+for region in $REGIONS; do
+  printf "="	
   for sg in $(aws ec2 describe-network-interfaces --region $region | jq --raw-output '.NetworkInterfaces[].Groups[].GroupId'); do
-    printf "%s\t%s\n" "$region" "$sg">>sg-used-eni.txt; printf "-";\
-  done;
+    printf "%s\t%s\n" "$region" "$sg">>sg-used-eni.txt
+    printf "-"
+  done
 done
 wc -l sg-used-eni.txt
 
 printf "\nstep 5: list security groups associated with elbs "
-for region in $(aws ec2 describe-regions | jq --raw-output '.Regions[].RegionName'); do
-  printf "=";	
+for region in $REGIONS; do
+  printf "="
   for sg in $(aws elb describe-load-balancers --region $region | jq --raw-output '.LoadBalancerDescriptions[].SecurityGroups[]'); do
-    printf "%s\t%s\n" "$region" "$sg">>sg-used-elb.txt; printf "-";\
-  done;
+    printf "%s\t%s\n" "$region" "$sg">>sg-used-elb.txt
+    printf "-"
+  done
 done
 wc -l sg-used-elb.txt
 
 printf "\nstep 6: list security groups associated with rds instances "
-for region in $(aws ec2 describe-regions | jq --raw-output '.Regions[].RegionName'); do
-  printf "=";	
+for region in $REGIONS; do
+  printf "="
   for sg in $(aws rds describe-db-security-groups --region $region | jq --raw-output '.DBSecurityGroups[].EC2SecurityGroups[].EC2SecurityGroupId'); do
-    printf "%s\t%s\n" "$region" "$sg">>sg-used-rds.txt; printf "-";
-  done;
+    printf "%s\t%s\n" "$region" "$sg">>sg-used-rds.txt
+    printf "-"
+  done
 done
 wc -l sg-used-rds.txt
 
+printf "\nstep 6b: list security groups associated with rds instance vpcs "
+for region in $REGIONS; do
+  printf "=" >&2
+  for sg in $(aws rds describe-db-instances --region $region | jq --raw-output '.DBInstances[].VpcSecurityGroups[].VpcSecurityGroupId'); do
+    printf "%s\t%s\n" "$region" "$sg"
+    printf "-" >&2
+  done
+done | sort -u > sg-used-rds-vpc.txt
+wc -l sg-used-rds-vpc.txt
+
 printf "\nstep 7: list security groups associated with lambda functions "
-for region in $(aws ec2 describe-regions | jq --raw-output '.Regions[].RegionName'); do
-  printf "=";	
+for region in $REGIONS; do
+  printf "="
   for sg in $(aws lambda list-functions --region $region | jq --raw-output '.Functions[].VpcConfig.SecurityGroupIds[]?'); do
-    printf "%s\t%s\n" "$region" "$sg">>sg-used-lambda.txt; printf "-";
-  done;
+    printf "%s\t%s\n" "$region" "$sg">>sg-used-lambda.txt
+    printf "-"
+  done
 done
 wc -l sg-used-lambda.txt
 
 printf "\nstep 8: generate a single list of unique used security groups "
-sort sg-used-* | uniq >sg-used-sorted.txt
-sort sg-all.txt | uniq >sg-all-sorted.txt
+sort -u sg-used-*  >sg-used-sorted.txt
+sort -u sg-all.txt >sg-all-sorted.txt
 wc -l sg-used-sorted.txt
 
 printf "\nstep 9: generate the list of unused security groups "
@@ -90,11 +110,11 @@ comm -23 sg-all-sorted.txt sg-used-sorted.txt >sg-unused.txt
 wc -l sg-unused.txt
 
 printf "\nstep 10. get the descriptions of the unused security groups "
-while IFS=$'\t' read -r region sg
-do
-  description=$(aws ec2 describe-security-groups --region $region --group-ids $sg | jq --raw-output '.SecurityGroups[].Description'); 
-  printf "%s\t%s\t%s\n" "$region" "$sg" "$description">>sg-unused-final.txt; printf "-";
+while read -r region sg; do
+  description=$(aws ec2 describe-security-groups --region $region --group-ids $sg | jq --raw-output '.SecurityGroups[].Description')
+  printf "%s\t%s\t%s\n" "$region" "$sg" "$description">>sg-unused-final.txt
+  printf "-"
 done <sg-unused.txt
 
-printf "\n";
-more sg-unused-final.txt
+printf "\n\n"
+cat sg-unused-final.txt
